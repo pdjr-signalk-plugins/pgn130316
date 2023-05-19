b3682bdea64229e9e16506db343c3943df407e1f
Paul Reeve <preeve@pdjr.eu>
2023-05-19 21:40:54 +0200

    Tweak

--- index.js
+++ index.js
@@ -86,6 +86,8 @@ module.exports = function(app) {
 
     if (options.temperatureMapping) {
 
+      log.N("waiting for messages");
+
       var nodes = new Set();
 
       app.emitPropertyValue('pgn-to-signalk', {
